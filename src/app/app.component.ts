37e988538313afb121f5656ba54f3095e39116f3
manikandanganapathy-github <manikandang@virtusa.com>
2018-01-18 11:59:16 +0530

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  userAnswer: string = '';
+  answerDisplay: string = '';
+  showSpinner: boolean = false;
+
+  showAnswer() {
+    this.showSpinner = true;
+
+    setTimeout(() => {
+      this.answerDisplay = this.userAnswer;
+      this.showSpinner = false;
+    }, 2000);
+  }
+
 }
